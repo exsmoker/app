b84db232b58bf66fabb868ccf19cd04a4dfbe2f1
vauxhall <zero.griffin@gmail.com>
2020-08-18 19:32:14 +0300

    Update craving view

--- exsmoker/Craving.swift
+++ exsmoker/Craving.swift
@@ -10,6 +10,55 @@ import SwiftUI
 
 struct Craving: View {
     var body: some View {
-        Circle()
+        NavigationView {
+            List {
+                Section(header:
+                    Text("What triggered it?")) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("I saw somebody smoking")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("I'm bored")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("I'm anxious'")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("I'm nervous")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("I just feel like it")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("I don't know")
+                            .padding()
+                            .foregroundColor(.pink)
+                    }
+                }
+            }.listStyle(GroupedListStyle())
+                .navigationBarTitle("Craving", displayMode: .large)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
