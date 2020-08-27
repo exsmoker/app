1583743b9dc8113a00ca96eeaea7debb203c1ddb
vauxhall <zero.griffin@gmail.com>
2020-08-27 10:57:57 +0200

    Add triggers

--- iOS/Triggers.swift
+++ iOS/Triggers.swift
@@ -1,19 +1,28 @@
 import SwiftUI
+import Core
 
 struct Triggers: View {
     let event: Event
     @Binding var visible: Event?
+    @State private var triggers = [[Smoke.Trigger]]()
     
     var body: some View {
         NavigationView {
-            List {
-                Section(header:
-                            Text("What.triggered")
-                            .foregroundColor(.secondary)) {
-                    Circle()
+            GeometryReader { geo in
+                ScrollView {
+                    ForEach(triggers, id: \.self) { item in
+                        HStack {
+                            Spacer()
+                            Text(.init(item.first!.title))
+                            Spacer()
+                            if item.count == 2 {
+                                Text(.init(item.last!.title))
+                                Spacer()
+                            }
+                        }.padding(.vertical, 6)
+                    }
                 }
-            }.listStyle(GroupedListStyle())
-            .navigationBarTitle(.init(event.name), displayMode: .large)
+            }.navigationBarTitle(.init(event.name), displayMode: .large)
             .navigationBarItems(trailing:
                                     Button(action: {
                                         visible = nil
@@ -23,6 +32,17 @@ struct Triggers: View {
                                             .foregroundColor(.secondary)
                                             .padding()
                                     })
+            .onAppear {
+                triggers = stride(from: 0, to: Smoke.Trigger.allCases.count, by: 2).reduce(into: []) {
+                    $0.append({
+                        var item = [Smoke.Trigger.allCases[$0]]
+                        if $0 + 1 < Smoke.Trigger.allCases.count {
+                            item.append(Smoke.Trigger.allCases[$0 + 1])
+                        }
+                        return item
+                    } ($1))
+                }
+            }
         }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
